a5ae3cc041fcf24ade63ed741dce2486564f06e9
polherron <pol.herron@gmail.com>
2019-06-15 19:57:04 +0100

    ArrayList added

--- SoftwareTestAutomationOOP/src/com/sta/ControllingClass.java
+++ SoftwareTestAutomationOOP/src/com/sta/ControllingClass.java
@@ -2,23 +2,34 @@ package com.sta;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ControllingClass {
 
 	public static void main(String[] args) {
-				
+			
+		ArrayList<Person> myPeople = new ArrayList<Person>();
 		Person person1 = new Person();
 		person1.idNo = 1234;
 		person1.fName = "Paul";
 		person1.lName = "Herron";
 		person1.address = "Derry";
 		
-		Person person2 = person1;
+		myPeople.add(person1);
 		
-		if (person1 == person2) {
-			System.out.print("People are equal");
-		} else {
-			System.out.print("People are not equal");
+		Person person2 = new Person();
+		person2.idNo = 12345;
+		person2.fName = "Michael";
+		person2.lName = "Doherty";
+		person2.address = "Dungloe";
+		
+		myPeople.add(person2);
+		
+		//Use a foreach loop
+		//We are going to use our toString method
+		for (Person person : myPeople) {
+			System.out.println(person.toString());
 		}
 
 	}
