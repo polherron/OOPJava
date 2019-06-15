4f46d704f78b08bcfc0099b8900c931bdff93310
polherron <pol.herron@gmail.com>
2019-06-15 17:31:44 +0100

    Added equals method to compare objects

--- SoftwareTestAutomationOOP/src/com/sta/Person.java
+++ SoftwareTestAutomationOOP/src/com/sta/Person.java
@@ -1,10 +1,16 @@
 package com.sta;
 
-import java.sql.Date;
-
 public class Person {
 	public int idNo;
 	public String fName;
 	public String lName;
 	public String address;
+	
+	public boolean equals(Person person)
+	{
+		return (this.idNo == person.idNo
+				&& this.fName == person.fName
+				&& this.lName == person.lName
+				&& this.address == person.address);
+	}
 }
